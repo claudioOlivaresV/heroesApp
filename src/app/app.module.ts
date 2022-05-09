7817ed0fd487533065116029bcff74f6cd50d610
Claudio Olivares <caolivares.3it@bancochile.cl>
2022-05-09 18:49:31 -0400

    se agrega estructura Ãbásica y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    AuthRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
